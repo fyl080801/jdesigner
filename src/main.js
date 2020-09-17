63fecd1e01213110560041bbf9c367206b10b398
fengyuanliang3 <fengyuanliang2@jd.com>
2020-09-17 17:51:30 +0800

    chore: 引用

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import JDesigner from '../lib'
+
 Vue.config.productionTip = false
+
+Vue.use(JDesigner)
 
 new Vue({
   router,
